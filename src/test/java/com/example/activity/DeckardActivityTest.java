3b4fd94eb61388c2ea662d15e6299c527863e26c
Christian Williams <christianw@google.com>
2017-10-27 16:18:20 -0700

    Use shadowOf() to make sure method overloading doesn't cause link problems.

--- src/test/java/com/example/activity/DeckardActivityTest.java
+++ src/test/java/com/example/activity/DeckardActivityTest.java
@@ -4,14 +4,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class DeckardActivityTest {
 
     @Test
     public void testSomething() throws Exception {
+        assertNotNull(shadowOf(RuntimeEnvironment.application));
         assertTrue(Robolectric.setupActivity(DeckardActivity.class) != null);
     }
 }
