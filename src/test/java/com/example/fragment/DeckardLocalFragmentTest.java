5fec4018a7197b204ffe3e4a9f1d0f12395309ab
Brett Chabot <brettchabot@google.com>
2018-10-25 14:40:44 -0700

    Add tests using FragmentScenario

---
+++ src/test/java/com/example/fragment/DeckardLocalFragmentTest.java
@@ -0,0 +1,33 @@
+package com.example.fragment;
+
+import com.example.R;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(AndroidJUnit4.class)
+public class DeckardLocalFragmentTest {
+
+    @Test
+    public void fragmentLaunch() {
+        FragmentScenario<DeckardFragment> scenario = FragmentScenario.launch(DeckardFragment.class);
+        assertThat(scenario).isNotNull();
+    }
+
+    @Test
+    public void fragmentLaunchWithEspresso() {
+        FragmentScenario<DeckardFragment> scenario = FragmentScenario.launch(DeckardFragment.class);
+        onView(withId(R.id.text)).check(matches(withText("Hello Fragment!")));
+    }
+}
